d5d57d56b58d04ba0f164663e5a5e70a95a362c1
Юрій Куник <kunyk.yuri@gmail.com>
2022-10-16 14:32:20 +0300

    Add project files.

---
+++ TestApplication/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using TestApplication.Models;
+
+namespace TestApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly folderdbContext _db;
+
+        public HomeController(ILogger<HomeController> logger,folderdbContext db)
+        {
+            _db = db;
+        }
+
+        [Route("{*catchall}")]
+        public IActionResult Index()
+        {
+            string path = HttpContext.Request.Path;
+            string str = path.Split("/").Last();
+            str = str.Replace("%20", " ");
+            ViewBag.Path = str;
+            List<string> subfolderURLs = new List<string>();
+        
+            Folder folder = _db.Folders.FirstOrDefault(f=>f.Name == str);
+            if(folder != null)
+            {
+                List<Folder> subfolders = _db.Folders.Where(f => f.RootFolderId == folder.Id).ToList();
+                if(subfolders.Count > 0)
+                {
+                    foreach(Folder subfolder in subfolders)
+                    {
+                        subfolderURLs.Add(HttpContext.Request.Path + "/" + subfolder.Name.Replace(" ","%20"));
+                    }
+                }
+                ViewBag.Folders = subfolders;
+            }
+
+            ViewBag.SubFolderURLs = subfolderURLs;
+
+            return View();
+        }
+
+    }
+}
---
+++ TestApplication/Models/Folder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace TestApplication.Models
+{
+    public partial class Folder
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? RootFolderId { get; set; }
+    }
+}
